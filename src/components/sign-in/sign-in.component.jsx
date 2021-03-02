999f547ce50081740a649d3d57b26851db7e4d90
Russell Hart <russellbot@gmail.com>
2021-03-02 14:34:54 -0500

    made google sign in a button to avoid form error message

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -62,7 +62,7 @@ const SignIn = () => {
           <CustomButton type="submit" value="Submit Form">
             SIGN IN
           </CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
             {" "}
             Sign in with Google{" "}
           </CustomButton>
