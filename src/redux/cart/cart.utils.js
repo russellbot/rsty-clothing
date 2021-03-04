60fa46db7a7f834b98d41aa4201ae161e49a54c1
Russell Hart <russellbot@gmail.com>
2021-03-04 14:22:30 -0500

    added addItem and removeItem functionality to checkout item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,33 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    // search cartItems to see if the cartItemToAdd is a duplicate
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
-    // if it is a duplicate, make a new array where we add +1 to the quantity of the matching cartItem
-    if (existingCartItem) {
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id
-                ? { ...cartItem, quantity: cartItem.quantity + 1 }
-                : cartItem
-            )
-    }
-    // if it is not a duplicate, add it to the end of the array with quantity of 1
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
-}
+  // search cartItems to see if the cartItemToAdd is a duplicate
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+  // if it is a duplicate, make a new array where we add +1 to the quantity of the matching cartItem
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  // if it is not a duplicate, add it to the end of the array with quantity of 1
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  // find the cart item to remove from the cartItems array
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+  // if quantity is 1, then remove the item from cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+  // if quantity is more than 1, subtract 1 from quantity
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
